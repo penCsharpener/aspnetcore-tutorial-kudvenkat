e0372de88a5021f9bcd25da0621bf4aa890e1e41
penCsharpener <matthiasmueller@protonmail.com>
2019-09-08 00:10:41 +0200

    configure service for DbContext

--- kudvenkat/Startup.cs
+++ kudvenkat/Startup.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using kudvenkat.DataAccess.Models;
 using kudvenkat.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace kudvenkat {
     public class Startup {
@@ -19,6 +22,8 @@ namespace kudvenkat {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContextPool<AppDbContext>(options => options.UseMySql(_config.GetConnectionString("EmployeeDBConnection")));
+
             services.AddMvc();
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
