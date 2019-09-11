9361494e19b3368f758a13f658acc3825f658dc1
penCsharpener <matthiasmueller@protonmail.com>
2019-09-11 22:49:05 +0200

    preparing user registration page

---
+++ kudvenkat/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kudvenkat.Controllers {
+    public class AccountController : Controller {
+
+        [HttpGet]
+        public IActionResult Register() {
+            return View();
+        }
+    }
+}
