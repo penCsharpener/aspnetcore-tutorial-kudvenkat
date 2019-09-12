abcc689889b4dee07470c9c0ba29621883a7ad3d
penCsharpener <matthiasmueller@protonmail.com>
2019-09-12 23:42:09 +0200

    update AccountController to store registered user in database

--- kudvenkat/Controllers/AccountController.cs
+++ kudvenkat/Controllers/AccountController.cs
@@ -2,14 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using kudvenkat.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace kudvenkat.Controllers {
     public class AccountController : Controller {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                                 SignInManager<IdentityUser> signInManager) {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
         [HttpGet]
         public IActionResult Register() {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model) {
+            if (ModelState.IsValid) {
+                var user = new IdentityUser {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded) {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach (var error in result.Errors) {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
