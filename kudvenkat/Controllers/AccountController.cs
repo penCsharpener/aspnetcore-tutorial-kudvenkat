148c21905b83b4c21827e97f85a9db24c9dd18ab
penCsharpener <matthiasmueller@protonmail.com>
2019-09-13 00:21:12 +0200

    implement login methods in AccountController

--- kudvenkat/Controllers/AccountController.cs
+++ kudvenkat/Controllers/AccountController.cs
@@ -44,6 +44,26 @@ namespace kudvenkat.Controllers {
             return View(model);
         }
 
+        [HttpGet]
+        public IActionResult Login() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model) {
+            if (ModelState.IsValid) {
+
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: model.RememberMe, false);
+
+                if (result.Succeeded) {
+                    return RedirectToAction("index", "home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid login attempt");
+            }
+            return View(model);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Logout() {
             await _signInManager.SignOutAsync();
--- kudvenkat/ViewModels/RegisterViewModel.cs
+++ kudvenkat/ViewModels/RegisterViewModel.cs
@@ -14,6 +14,7 @@ namespace kudvenkat.ViewModels {
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
