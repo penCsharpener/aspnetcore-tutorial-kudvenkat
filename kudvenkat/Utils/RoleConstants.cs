a805dd85b0c4f79eeb53c08c257460e45c29b07d
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2019-09-25 22:03:38 +0200

    add RoleConstants

---
+++ kudvenkat/Utils/RoleConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kudvenkat.Utils {
+    public static class RoleConstants {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string SuperAdmin = "Super Admin";
+    }
+}
